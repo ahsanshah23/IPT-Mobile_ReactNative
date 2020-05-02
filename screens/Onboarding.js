a602e19c26f4a9dd6d6780660f541971999588e5
Rabiya Salman <rabiya.salman0015@gmail.com>
2020-05-02 05:33:47 +0500

    defense by jury

--- screens/Onboarding.js
+++ screens/Onboarding.js
@@ -35,11 +35,13 @@ export default class Onboarding extends React.Component {
     //       console.warn(users[0]);
     //       if (users[0].Role == "Student") {
     //         AsyncStorage.setItem('email', email);
-            AsyncStorage.setItem('ID', 163942);
-            this.props.navigation.navigate('Student_Home');
+            // AsyncStorage.setItem('ID', 163942);
+            // this.props.navigation.navigate('Student_Home');
   //         }
   //         else if (users[0].Role == "Teacher") {
   //           AsyncStorage.setItem('email',email);
+  AsyncStorage.setItem('ID', 163942);
+            this.props.navigation.navigate('Teacher_Home');
   //           AsyncStorage.setItem('name', users[0].Name);
   //           this.props.navigation.navigate('Teacher_Home');
   //         }
